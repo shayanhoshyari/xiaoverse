4bb129ee8c8da01d585bfd3539b03612203d3701
Shayan Hoshyari <hoshyari@adobe.com>
2025-01-07 19:19:30 -0800

    Add the template

---
+++ leetcode/template/main.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/binary-search
+
+import pytest
+import sys
+from typing import Any
+
+def template(*args: Any) -> Any:
+    ...
+
+def test_template() -> None:
+    ...
+
+if __name__ == "__main__":
+    sys.exit(pytest.main(["-vv", __file__]))
