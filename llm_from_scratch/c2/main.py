58636a9b4c370011cdcdf21b681590c892f0ac82
Shayan Hoshyari <hoshyari@adobe.com>
2025-02-01 23:06:45 -0800

    UPdate

--- llm_from_scratch/c2/main.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-VERDICT = Path(__file__).with_name("the-verdict.txt")
-
-with open(VERDICT, "r", encoding="utf-8") as f:
-    raw_text = f.read()
-
-print("Total number of character:", len(raw_text))
-print(raw_text[:99])
--- llm_from_scratch/notebook.py
+++ llm_from_scratch/notebook.py
@@ -1,4 +1,23 @@
 from jupyter_server import serverapp
+# import sys
+# from pathlib import Path
 
 if __name__ == "__main__":
+    # This is dirtying the output, which might have some unwanted side-effect
+    # but is needed for vscode intellisense to work
+    # python_version = f"python{sys.version_info.major}.{sys.version_info.minor}"
+    # lib =  Path(sys.executable).parents[1] / f"lib/{python_version}"
+    # pylance_cue = lib / "site-packages/pylance-cue.txt"
+
+    # site = lib / "site.py"
+    # content = site.read_text()
+    # site.unlink()
+    # site.write_text(content)
+
+    # sys_path = [path for path in sys.path if path] # remove ""
+    # pylance_cue.write_text("\n".join(sys_path))
+
+    # print("Python is", sys.executable)
+    # print("Wrote pylance cue to", pylance_cue)
+
     serverapp.main(open_browser=False, token="", disable_check_xsrf=True)
