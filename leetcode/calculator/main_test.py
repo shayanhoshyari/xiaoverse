50b9c9110a3c79a1e78c079ce6069850d05572b8
Shayan Hoshyari <hoshyari@adobe.com>
2025-03-23 23:05:02 -0700

    Add calculator boilerplate

---
+++ leetcode/calculator/main_test.py
@@ -0,0 +1,21 @@
+"""
+https://leetcode.com/problems/basic-calculator
+"""
+
+import sys
+from typing import Any
+
+import pytest
+
+
+def template(*args: Any) -> Any:
+    pass
+
+
+@pytest.mark.timeout(3)
+def test_template() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(pytest.main(["-vv", __file__]))
